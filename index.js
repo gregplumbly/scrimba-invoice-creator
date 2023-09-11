595e914b2f9133e1567d210882fa621d0e604a60
Greg Plumbly <greg.plumbly@gmail.com>
2023-09-11 20:19:14 +0100

    minor layout update

--- index.js
+++ index.js
@@ -22,9 +22,8 @@ function renderItems() {
       const invoiceItem = document.createElement("div");
       invoiceItem.classList.add("invoice-item");
       invoiceItem.innerHTML = `
-      <p class="line-item" >${task}</p>
-       <button class="delete-btn" id="delete-btn" data-index=${index}>X</button>
-      <p class="line-item-price">$<span class="line-item-price-amt" id="line-item-price-amt">${price}</span></p>
+      <p class="line-item" >${task} <span class="delete-btn" id="delete-btn" data-index=${index}>Remove item</span></p>
+\      <p class="line-item-price">$<span class="line-item-price-amt" id="line-item-price-amt">${price}</span></p>
       `;
       invoiceItems.appendChild(invoiceItem);
       sendInvoiceBtn.classList.remove("btn-disabled");
